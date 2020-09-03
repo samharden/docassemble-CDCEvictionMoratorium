20d8ec305a728ffe48290c84a5037a55d0d5aea7
System Administrator <qsteenhuis@gmail.com>
2020-09-02 21:14:56 -0400

    Fix error of law (too narrow). Add examples. Explain additional rights in some states.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.CDCEvictionMoratorium',
-      version='0.0.4',
+      version='0.0.5',
       description=('Online form to complete CDC Eviction Moratorium letter'),
       long_description='# docassemble.CDCEvictionMoratorium\r\n\r\nOnline form to complete CDC Eviction Moratorium letter\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
